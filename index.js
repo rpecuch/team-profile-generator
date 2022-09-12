41ab4de90f9e753fd4284c790f744508a8acfcad
userrp <109092365+rpecuch@users.noreply.github.com>
2022-09-12 19:46:35 -0400

    created separate files for classes, started writing function to generate html, wrote function to take answers and create a new manager object

--- index.js
+++ index.js
@@ -1,77 +1,25 @@
 const inquirer = require('inquirer');
-
-class Employee {
-    constructor(name, id, email) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
-    }
-    getName() {
-        return this.name;
-    }
-    getId() {
-        return this.id;
-    }
-    getEmail() {
-        return this.email;
-    }
-    getRole() {
-        return "Employee";
-    }
-}
-
-class Manager extends Employee {
-    constructor(name, id, email, officeNumber) {
-        super(name, id, email);
-        this.officeNumber = officeNumber;
-    }
-    getRole() {
-        return "Manager";
-    }
-}
-
-class Engineer extends Employee {
-    constructor(name, id, email, github) {
-        super(name, id, email);
-        this.github = github;
-    }
-    getGithub() {
-        return this.github;
-    }
-    getRole() {
-        return "Engineer";
-    }
-}
-
-class Intern extends Employee {
-    constructor(name, id, email, school) {
-        super(name, id, email);
-        this.school = school;
-    }
-    getSchool(){
-        return this.school;
-    }
-    getRole() {
-        return "Intern";
-    }
-}
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
 inquirer
     .prompt([
         {
             type: 'input',
             message: 'Enter team manager name:',
-            name: 'name',
+            name: 'managerName',
         },
         {
             type: 'input',
             message: "Enter team manager's employee ID:",
-            name: 'id',
+            name: 'managerId',
         },
         {
             type: 'input',
             message: "Enter team manager's email address:",
-            name: 'email',
+            name: 'managerEmail',
         },
         {
             type: 'input',
@@ -131,7 +79,60 @@ inquirer
         //now take back to menu to select team member to add
         //html generated when team finished building
     ])
+    //send answers to function where objects are created
+    .then()
+    .catch(error => console.log(error));
 
-    //take values from answers object and put into the classes you made
-    
-    //function to generate html
+//take values from answers object and put into the classes you made
+newFunction = function(answers) {
+    manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.officeNumber);
+
+}
+
+//function to generate html
+generateHtml = function() {
+    const head = `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta name="Description" content="Enter your description here" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+        />
+        <link rel="stylesheet" href="assets/css/style.css" />
+        <title>My Team</title>
+      </head>\n`;
+    const jumbo = `<body>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4 text-center">My Team</h1>
+        </div>
+      </div>\n`
+    const managerCard = `<div class="row justify-content-center">
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">${manager.name}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Manager</h6>
+          <p class="card-text">ID: ${manager.id}</p>
+          <p>Email: ${manager.email}</p>
+          <p>Office number: ${manager.officeNumber}</p>
+        </div>
+      </div>\n`
+    //cards for the employees
+    const end = `</div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+  </body>
+</html>`
+    //also need to return employee cards
+    return(head, jumbo, managerCard, end);
+}
