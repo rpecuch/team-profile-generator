4714ea5ad48fa344df8cb4c51f05409f35fe8fb8
userrp <109092365+rpecuch@users.noreply.github.com>
2022-09-12 18:57:43 -0400

    created employee classes

---
+++ index.js
@@ -0,0 +1,55 @@
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    getName() {
+        return this.name;
+    }
+    getId() {
+        return this.id;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getRole() {
+        return "Employee";
+    }
+}
+
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email);
+        this.officeNumber = officeNumber;
+    }
+    getRole() {
+        return "Manager";
+    }
+}
+
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        super(name, id, email);
+        this.github = github;
+    }
+    getGithub() {
+        return this.github;
+    }
+    getRole() {
+        return "Engineer";
+    }
+}
+
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+        super(name, id, email);
+        this.school = school;
+    }
+    getSchool(){
+        return this.school;
+    }
+    getRole() {
+        return "Intern";
+    }
+}
